e1ab5ebe40e49853126eb94061225faf9aa9eb5a
alsami6 <alsami1209@gmail.com>
2024-11-26 21:04:08 +0600

    _

--- module_5/String_Capacity_Functions.cpp
+++ module_5/String_Capacity_Functions.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 
 int main(){
-    string s = "Hello";
-    cout << s.size() << endl;
+    string s = "Hello I am Sami What's goning on there.";
+
+    s.resize(50,'X');
+
+    cout << s << endl;
 
     return 0;
 }
--- module_5/String_in_C++.cpp
+++ module_5/String_in_C++.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main(){
-    
+    string s = "Hello";
+    s = "Hi Ami Sami kmn aso tomra ?";
+
+    cout << s << endl;
 
     return 0;
 }
