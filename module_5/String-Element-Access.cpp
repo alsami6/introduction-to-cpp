76dfd29c4dc88f8fa406bef4fadf4d561aec8f58
alsami6 <alsami1209@gmail.com>
2024-11-26 23:04:33 +0600

    _

--- module_5/String-Element-Access.cpp
+++ module_5/String-Element-Access.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 
 int main(){
-    
+    string s;
+    cin >> s;
+    cout << s[0] << endl;
+    cout << s.at(8) << endl;
+    cout << s.back() << endl;
+    cout << s.front() << endl;
 
     return 0;
 }
---
+++ module_5/String-Modifiers-Part-I.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s = "Hello World";
+    // string s2 = "Hello";
+    // s[1] = 'u';
+    // s.pop_back();
+    s.push_back('S');
+
+    // s.append(s2);
+    // s.push_back('A');
+    cout << s << endl;
+
+    return 0;
+}
