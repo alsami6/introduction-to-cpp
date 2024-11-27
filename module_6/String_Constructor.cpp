b5af489170119f8ddc269012d23cf17cc0c55f8c
alsami6 <alsami1209@gmail.com>
2024-11-27 23:27:52 +0600

    _

---
+++ module_6/String_Constructor.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // string s = "Hello World";
+    // string t(s,4);
+
+    string t(6,'A');
+
+    cout << t << endl;
+
+    return 0;
+}
