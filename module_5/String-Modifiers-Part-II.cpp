19539a3815cb448ff81bdb6b3e31026673464297
alsami6 <alsami1209@gmail.com>
2024-11-27 14:46:52 +0600

    _

---
+++ module_5/String-Modifiers-Part-II.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s = "Hello World";
+    // string s2 = "Hi";
+
+    // s.assign("Gello");
+
+    // s.erase(3,2);
+
+    // s.replace(6,0,"Bangladesh ");
+
+    s.insert(6,"Sami ");
+
+    cout << s << endl;
+
+    return 0;
+}
